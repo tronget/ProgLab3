e8c11c57639bec5c69724c119924fce10edd0aea
tronget <99277521+tronget@users.noreply.github.com>
2023-11-20 16:18:57 +0300

    Update Znayka.java

--- classes/persons/Znayka.java
+++ classes/persons/Znayka.java
@@ -8,20 +8,18 @@ public class Znayka extends Person {
     public Znayka() {
         super("Знайка");
     }
+    
+    @Override
     public void turnOnTheElectricity(Electricity electricity) {
         if (!(getMood().equals("пристыженный"))) {
-            electricity.toggle();
-            System.out.printf("%s включил электричество.\n", name);
+            super.turnOnTheElectricity(electricity);
         } else {
             System.out.printf("%s не зажег электричество, так как ему было стыдно.\n", name);
         }
     }
+    
     public void beAshamed() {
         setMood(Mood.ASHAMED);
         System.out.printf("%s стыдится.\n", name);
-    }
-    public void goToBed(Bed bed) {
-        bed.setPersonOnBed(this);
-        System.out.println("Знайка с размаху бросился на кровать.\n");
     }
 }
