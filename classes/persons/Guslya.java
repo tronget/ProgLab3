487ce8bae43c63b4da4b7ff412cb296716c6a842
tronget <99277521+tronget@users.noreply.github.com>
2023-11-20 16:23:57 +0300

    Update Guslya.java

--- classes/persons/Guslya.java
+++ classes/persons/Guslya.java
@@ -4,8 +4,4 @@ public class Guslya extends Person {
     public Guslya() {
         super("Гусля");
     }
-
-    public void speak() {
-        System.out.printf("%s сказал что-то, что всех рассмешило.\n", name);
-    }
 }
