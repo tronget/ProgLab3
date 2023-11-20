c749ec3f0b245804770b07af2691c8f5ae20cb2f
tronget <99277521+tronget@users.noreply.github.com>
2023-11-20 16:50:48 +0300

    Update Guslya.java

--- classes/persons/Guslya.java
+++ classes/persons/Guslya.java
@@ -1,7 +1,14 @@
 package classes.persons;
 
 public class Guslya extends Person {
-    public Guslya() {
+    private static Guslya instance;
+    public static synchronized Guslya getInstance() {
+        if (instance == null) {
+            instance = new Guslya();
+        }
+        return instance;
+    }
+    private Guslya() {
         super("Гусля");
     }
 }
