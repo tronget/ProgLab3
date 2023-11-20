980fa2603a6a8c52f47053a81c0c553e813aab7a
tronget <99277521+tronget@users.noreply.github.com>
2023-11-20 15:20:03 +0300

    Create Location.java

---
+++ classes/location/Location.java
@@ -0,0 +1,37 @@
+package classes.location;
+
+import java.util.Objects;
+
+public abstract class Location {
+    protected String name;
+
+    public Location() {
+        this.name = "без имени";
+    }
+
+    public Location(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return Objects.equals(name, location.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Локация - %s.\n".formatted(name);
+    }
+}
