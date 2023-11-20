8806fafe32616fe4c7a714714f7db43fcabd8176
tronget <99277521+tronget@users.noreply.github.com>
2023-11-20 16:34:49 +0300

    Update Crowd.java

--- classes/persons/Crowd.java
+++ classes/persons/Crowd.java
@@ -4,13 +4,8 @@ import classes.location.InnerLocation;
 import enums.Mood;
 
 public class Crowd extends Person {
-    public Crowd() {
-        super("Все, кроме Знайки");
-    }
-    public void laughAtZnayka(Znayka znayka) {
-        System.out.printf("%s смеются над Знайкой.\n", name);
-        this.setMood(Mood.FUNNY);
-        znayka.beAshamed();
+    public Crowd(String name) {
+        super(name);
     }
     public void pushEachOther() {
         System.out.printf("%s толкаются.\n", name);
